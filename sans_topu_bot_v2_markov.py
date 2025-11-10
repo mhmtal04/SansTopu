ea89d142512d09e5137e353f249032b77f6e605d
mhmtal04 <mhmtal04@gmail.com>
2025-11-10 09:21:18 +0300

    Increase trials in generate_predictions function

--- sans_topu_bot_v2_markov.py
+++ sans_topu_bot_v2_markov.py
@@ -81,7 +81,7 @@ def train_gradient_boost(df):
 
 # ------------------- Tahmin Fonksiyonu -------------------
 
-def generate_predictions(df, single_prob, cond_prob, nb_model, gb_model, markov_probs, pair_freq, n_preds=1, trials=5000):
+def generate_predictions(df, single_prob, cond_prob, nb_model, gb_model, markov_probs, pair_freq, n_preds=1, trials=1000000):
     """Ana tahmin üretme fonksiyonu"""
     predictions = []
     numbers_list = list(range(1, 35))
